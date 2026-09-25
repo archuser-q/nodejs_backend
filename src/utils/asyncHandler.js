48f3f5f834320486dfa2ef2dafdeff2ad62e1024
archuser-q <availableemailaddress123@tutamail.com>
2026-09-25 18:07:33 +0700

    nuke

--- src/utils/asyncHandler.js
+++
@@ -1,7 +0,0 @@
-// Bọc controller async để tự động forward lỗi cho errorHandler, tránh phải
-// try/catch lặp lại ở từng controller.
-module.exports = function asyncHandler(fn) {
-  return function wrapped(req, res, next) {
-    Promise.resolve(fn(req, res, next)).catch(next);
-  };
-};
